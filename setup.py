af2d7ffa8e7fb13af8f2000382411ce4c30fe2e3
Matt Chapman <matt@ninjitsuweb.com>
2019-03-12 19:31:08 -0700

    Use environment tags.

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from setuptools import setup, find_packages
 from distutils.core import Extension
-import sys
-if sys.version_info[0] < 3:
-    avro = 'avro'
-else:
-    avro = 'avro-python3'
+import platform
+import os
+
+INSTALL_REQUIRES = ["futures; python_version<'3.3'",
+                    "enum34; python_version<'3.5'",
+                    'requests']
 
 module = Extension('confluent_kafka.cimpl',
                    libraries=['rdkafka'],
@@ -14,15 +15,16 @@ module = Extension('confluent_kafka.cimpl',
                             'confluent_kafka/src/Producer.c',
                             'confluent_kafka/src/Consumer.c'])
 
+
 setup(name='confluent-kafka',
       version='0.11.0',
       description='Confluent\'s Apache Kafka client for Python',
       author='Confluent Inc',
       author_email='support@confluent.io',
-      url='https://github.com/confluentinc/confluent-kafka-python',
+      url='https://github.com/matt2000/confluent-kafka-python',
       ext_modules=[module],
       packages=find_packages(exclude=("tests",)),
       data_files=[('', ['LICENSE'])],
       extras_require={
-          'avro': ['fastavro', 'requests', avro]
+          'avro': ['fastavro', 'requests', "avro; python_version<'3'", "avro-python3; python_version>='3'"],
       })
